17975ed988c98a0f1806363ae478fd79c22921ef
wasit7 <wasit7@gmail.com>
2017-08-24 16:43:49 +0700

    fix the pendulum problem

--- week02/pendulum.py
+++ week02/pendulum.py
@@ -16,34 +16,43 @@ import scipy.integrate as integrate
 import matplotlib.animation as animation
 
 fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(0, 300), ylim=(-2, 50))
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
 ax.grid()
 
 line, = ax.plot([], [], 'o', lw=2)
-time_template = 'time = %.1fs'
+time_template = 'time:%.1fs, theta: %.1f'
 time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
 fps=25
 dt=1.0/fps
+def d2r(x):
+  return x/180.0*np.pi
+def r2d(x):
+  return x/np.pi*180.0
+
 #### begin simulation
-sx=0
-sy=0
-ux=40
-uy=30
-t=0.0
-#dt=0.1
-g=-9.8
+theta= d2r(45) #angular displacement
+omega= d2r(0)  #angular velocity
+alpha= d2r(0)  #angular acceleration
+m= 2.0 #kg
+L= 1.0 #m
+I= 4.0 #kg.m^2
+g=9.8
+t=0
 posx=[0]
 posy=[0]
-while sy>=0:
+list_theta=[theta]
+for i in range(200):
     t+=dt #t=t+dt
-    ux=ux
-    sx=sx+ux*dt
-    uy=uy+g*dt
-    sy=sy+uy*dt
+    alpha=m*g*np.sin(theta)*L
+    omega=omega+alpha*dt
+    theta=theta+omega*dt
+    sx=-L*np.sin(theta)
+    sy=L*np.cos(theta)
     posx.append(sx)
     posy.append(sy)
-print("ux:{} uy:{} sx:{} sy:{}".format(ux,uy,sx,sy))
+    list_theta.append(r2d(theta))
+
 #### end simulation
 def init():
     line.set_data([], [])
@@ -56,7 +65,7 @@ def animate(i):
     thisy = [0, posy[i]]
 
     line.set_data(thisx, thisy)
-    time_text.set_text(time_template % (i*dt))
+    time_text.set_text(time_template % (i*dt,list_theta[i]))
     return line, time_text
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, len(posx)),
